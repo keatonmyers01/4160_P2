a07d071b5c93cd25b434de01b7c756fbb9862503
keatonmyers01 <kjmyers@clemson.edu>
2023-05-03 18:41:49 -0400

    enemy logic in process

--- game/enemy.py
+++ game/enemy.py
@@ -4,6 +4,8 @@ from pygame import Rect, Surface, Color
 from engine.entity import LivingEntity
 from engine.location import Location
 
+import random
+
 FONT = pygame.font.SysFont("comicsansms", 16, True)
 
 
@@ -12,9 +14,24 @@ class Enemy(LivingEntity):
     def __init__(self, color: Color, mouse_pos: tuple[int, int]):
         super().__init__(Location(mouse_pos[0], mouse_pos[1]), priority=10, health=200, health_bar=True)
         self.color = color
+        self._type = random.randint(0, 1)
+        self._target_timer = 0
+
+    def melee_logic(self):
+        pass
+
+    def ranged_logic(self):
+        pass
 
     def tick(self, tick_count: int) -> None:
         super().tick(tick_count)
+
+        #doing this to make an easy difference between melee and ranged enemies
+        match self._type:
+            case 0:
+                pass
+            case 1:
+                pass
 
     def draw(self, surface: Surface) -> None:
         super().draw(surface)
